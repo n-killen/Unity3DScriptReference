b1958b21159ef47b1c78c46e92811f60b1f81415
n-killen <nicholas_killen@hotmail.com>
2013-11-09 10:45:15 -0700

    Added threaded search
    Much cleaner user experience; spin off several threads using permutations of the user's search string to find the best match in Unity's documentation. Falls back to a reference search of the original text if nothing is found.

--- Unity3DScriptReference.py
+++ Unity3DScriptReference.py
@@ -1,11 +1,13 @@
 # written by Jacob Pennock (www.jacobpennock.com)
 # based on WordPress Codex Search Plugin by Matthias Krok (www.welovewordpress.de)
+# modified by Nicholas Killen (11/9/2013)
 
 # available commands
 #   unity_reference_open_selection
 #   unity_reference_search_selection
 #   unity_reference_search_from_input
 
+import multiprocessing
 import sublime
 import sublime_plugin
 
@@ -20,26 +22,57 @@ def OpenUnityFunctionReference(text):
     url = 'http://docs.unity3d.com/Documentation/ScriptReference/' + text.replace(' ','%20') + '.html'
     webbrowser.open_new_tab(url)
 
+def crawl(result_queue, text):
+    import urllib2
+    try:
+        data = urllib2.urlopen('http://docs.unity3d.com/Documentation/ScriptReference/' + text.replace(' ','%20') + '.html').read()
+    except urllib2.URLError, e:
+        if e.code >= 400:
+            result_queue.close() #signal that thread has nothing more to contribute
+
+    result_queue.put(text) # send successful result back to parent thread
+
 class UnityReferenceOpenSelectionCommand(sublime_plugin.TextCommand):
     def run(self, edit):
+        processs = []
+        result_queue = multiprocessing.Queue()
+
         for selection in self.view.sel():
             # if the user didn't select anything, search the currently highlighted word
             if selection.empty():
                 selection = self.view.word(selection)
 
             text = self.view.substr(selection)
-            
-            # test for function and adjust string to match unity HTML scheme
-            if text.find('(') == -1:
-                n = text.find('.') + 1
-                if text[n].isupper() == False:
-                    text = text.replace('.', '-')
-                    text = text[0].capitalize() + text[1:]
-            else:
-                text = text.replace('(','')
-                text = text[0].capitalize() + text[1:]
-                
-            OpenUnityFunctionReference(text)
+
+            # spin off first thread to search for the unmodified selection
+            firstProcess = multiprocessing.Process(target=crawl, args=[result_queue, text])
+            firstProcess.start()
+            processs.append(firstProcess)
+
+            # second thread to search with capitalized selection
+            secondText = text[0].capitalize() + text[1:]
+
+            secondProcess = multiprocessing.Process(target=crawl, args=[result_queue, secondText])
+            secondProcess.start()
+            processs.append(secondProcess)
+
+            # third thread to search for attributes instead of functions
+            thirdText = text.replace('.', '-')
+            thirdText = thirdText[0].capitalize() + thirdText[1:]
+
+            thirdProcess = multiprocessing.Process(target=crawl, args=[result_queue, thirdText])
+            thirdProcess.start()
+            processs.append(thirdProcess)
+
+            # wait a short time for any results from threads
+            try:
+                result = result_queue.get(True, 1) # waits until any of the proccess have `.put()` a result
+                OpenUnityFunctionReference(result)
+            except:
+                SearchUnityScriptReferenceFor(text)
+
+            for process in processs: # kill off the threads
+                process.terminate()
 
 class UnityReferenceSearchSelectionCommand(sublime_plugin.TextCommand):
     def run(self, edit):
@@ -76,4 +109,6 @@ class UnityReferenceSearchFromInputCommand(sublime_plugin.WindowCommand):
 
     def on_cancel(self):
         pass
-    
+
+
+        
